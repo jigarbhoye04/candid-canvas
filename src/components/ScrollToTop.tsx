cfe076403ff57de5f3a69434c646a31c4078214a
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-07-18 23:15:53 +0530

    scrollTop update

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,43 +1,42 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 import "./components.css";
 import { FaCircleArrowUp } from "react-icons/fa6";
 import { FaArrowCircleUp } from "react-icons/fa";
+import { RiArrowUpSLine } from "react-icons/ri";
 
 const ScrollToTop = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    
-    useEffect(() => {
-        const toggleVisibility = () => {
-        if (window.scrollY > 300) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-        };
-    
-        window.addEventListener("scroll", toggleVisibility);
-    
-        return () => window.removeEventListener("scroll", toggleVisibility);
-    }, []);
-    
-    const scrollToTop = () => {
-        window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-        });
-    };
-    
-    return (
-        <div
-        className={`scroll-to-top ${isVisible ? "visible" : ""}`}
-        onClick={scrollToTop}
-        >
-        {/* <FaCircleArrowUp /> */}
-        <FaArrowCircleUp />
+   const [isVisible, setIsVisible] = useState(false);
 
-        </div>
-    );
+   useEffect(() => {
+      const toggleVisibility = () => {
+         if (window.scrollY > 300) {
+            setIsVisible(true);
+         } else {
+            setIsVisible(false);
+         }
+      };
+
+      window.addEventListener("scroll", toggleVisibility);
+
+      return () => window.removeEventListener("scroll", toggleVisibility);
+   }, []);
+
+   const scrollToTop = () => {
+      window.scrollTo({
+         top: 0,
+         behavior: "smooth",
+      });
+   };
+
+   return (
+      <div
+         className={`scroll-to-top ${isVisible ? "visible" : ""}`}
+         onClick={scrollToTop}
+      >
+         <RiArrowUpSLine />
+      </div>
+   );
 };
 
 export default ScrollToTop;
